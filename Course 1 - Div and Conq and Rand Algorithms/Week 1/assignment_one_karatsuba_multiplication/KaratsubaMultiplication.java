540757d61621a761f3318e38cb52bd5185f8db94
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-15 23:03:03 -0400

    Replaced tabs with spaces

--- Course 1 - Div and Conq and Rand Algorithms/Week 1/assignment_one_karatsuba_multiplication/KaratsubaMultiplication.java
+++ Course 1 - Div and Conq and Rand Algorithms/Week 1/assignment_one_karatsuba_multiplication/KaratsubaMultiplication.java
@@ -4,34 +4,34 @@ import java.math.BigInteger;
 
 public class KaratsubaMultiplication {
 
-	private KaratsubaMultiplication() {} // this class cannot be instantiated
+    private KaratsubaMultiplication() {} // this class cannot be instantiated
 
-	public static BigInteger multiply(BigInteger x, BigInteger y) {
-		// cutoff to brute force
-		int N = Math.max(x.bitLength(), y.bitLength());
-		if (N <= 1) return x.multiply(y);
+    public static BigInteger multiply(BigInteger x, BigInteger y) {
+        // cutoff to brute force
+        int N = Math.max(x.bitLength(), y.bitLength());
+        if (N <= 1) return x.multiply(y);
 
-		// number of bits divided by 2, rounded up
-		N = (N / 2) + (N % 2);
+        // number of bits divided by 2, rounded up
+        N = (N / 2) + (N % 2);
 
-		// x = a + 2^N b, y = c + 2^N d
-		BigInteger b = x.shiftRight(N);
-		BigInteger a = x.subtract(b.shiftLeft(N));
-		BigInteger d = y.shiftRight(N);
-		BigInteger c = y.subtract(d.shiftLeft(N));
+        // x = a + 2^N b, y = c + 2^N d
+        BigInteger b = x.shiftRight(N);
+        BigInteger a = x.subtract(b.shiftLeft(N));
+        BigInteger d = y.shiftRight(N);
+        BigInteger c = y.subtract(d.shiftLeft(N));
 
-		// compute sub-expressions
-		BigInteger ac = multiply(a, c);
-		BigInteger bd = multiply(b, d);
-		BigInteger abcd = multiply(a.add(b), c.add(d));
+        // compute sub-expressions
+        BigInteger ac = multiply(a, c);
+        BigInteger bd = multiply(b, d);
+        BigInteger abcd = multiply(a.add(b), c.add(d));
 
-		return ac.add(abcd.subtract(ac).subtract(bd).shiftLeft(N)).add(bd.shiftLeft(2 * N));
-	}
-	
-	public static void main(String[] args) {
-		BigInteger A = new BigInteger("3141592653589793238462643383279502884197169399375105820974944592");
-		BigInteger B = new BigInteger("2718281828459045235360287471352662497757247093699959574966967627");
-		BigInteger C = multiply(A, B);
-		System.out.println(C);
-	}
+        return ac.add(abcd.subtract(ac).subtract(bd).shiftLeft(N)).add(bd.shiftLeft(2 * N));
+    }
+    
+    public static void main(String[] args) {
+        BigInteger A = new BigInteger("3141592653589793238462643383279502884197169399375105820974944592");
+        BigInteger B = new BigInteger("2718281828459045235360287471352662497757247093699959574966967627");
+        BigInteger C = multiply(A, B);
+        System.out.println(C);
+    }
 }
--- Course 1 - Div and Conq and Rand Algorithms/Week 1/mergesort/MergeSort.java
+++ Course 1 - Div and Conq and Rand Algorithms/Week 1/mergesort/MergeSort.java
@@ -2,36 +2,36 @@ package mergesort;
 
 public class MergeSort {
 
-	// This class should not be instantiated.
-	private MergeSort() {}
+    // This class should not be instantiated.
+    private MergeSort() {}
 
-	// Stable merging of a[lo ... mid] with a[mid + 1 ... hi] by copying to aux[lo .. hi]
-	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
-		int i = lo, j = mid + 1;
-		for (int k = lo; k <= hi; k++) {
-			if (i > mid) a[k] = aux[j++];
-			else if (j > hi) a[k] = aux[i++];
-			else if (aux[i].compareTo(aux[j]) <= 0) a[k] = aux[i++];
-			else a[k] = aux[j++];
-		}
-	}
-	
-	// recursively call sort until the lo is greater than or equal to hi (array of size 0)
-	// uses an auxiliary array to copy elements for merging 
-	private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
-		if (lo >= hi) return;
-		int mid = lo + (hi - lo) / 2; // to prevent integer overflow
-		sort(aux, a, lo, mid);
-		sort(aux, a, mid + 1, hi);
-		merge(a, aux, lo, mid, hi);
-	}
-	
-	/**
-	 * Sorts the array in ascending order, using the natural order
-	 * @param a the array to be sorted
-	 */
-	public static void sort(Comparable[] a) {
-		Comparable[] aux = a.clone();
-		sort(a, aux, 0, a.length - 1);
-	}
+    // Stable merging of a[lo ... mid] with a[mid + 1 ... hi] by copying to aux[lo .. hi]
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (aux[i].compareTo(aux[j]) <= 0) a[k] = aux[i++];
+            else a[k] = aux[j++];
+        }
+    }
+    
+    // recursively call sort until the lo is greater than or equal to hi (array of size 0)
+    // uses an auxiliary array to copy elements for merging 
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (lo >= hi) return;
+        int mid = lo + (hi - lo) / 2; // to prevent integer overflow
+        sort(aux, a, lo, mid);
+        sort(aux, a, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+    
+    /**
+     * Sorts the array in ascending order, using the natural order
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = a.clone();
+        sort(a, aux, 0, a.length - 1);
+    }
 }
